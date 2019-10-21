5c5f23d0dc306202c215cddde32115c262cf4a73
Johnatan <djohnat_13@hotmail.com>
2019-10-21 17:13:25 -0400

    Cours16 Shader

---
+++ Cours16_Shader/Assets/CameraEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffect : MonoBehaviour {
+    private Material material;
+	// Use this for initialization
+	void Start () {
+        material = new Material(Shader.Find("Custom/TestEffectShader"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination,material);
+    }
+}
